fe359e59261e0959c22287f38293db7c6c701fd2
3arthqu4ke <hanspeteroliver.mueller@gmail.com>
2020-11-04 11:09:36 +0100

    fix damage calc for webs and beds

--- src/main/java/me/earth/earthhack/impl/util/minecraft/DamageUtil.java
+++ src/main/java/me/earth/earthhack/impl/util/minecraft/DamageUtil.java
@@ -5,6 +5,8 @@ import me.earth.earthhack.impl.util.thread.EnchantmentUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBed;
 import net.minecraft.block.BlockWeb;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
@@ -12,8 +14,10 @@ import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.CombatRules;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.Explosion;
@@ -58,7 +62,11 @@ public class DamageUtil implements Globals
 
     /**
      * Calculates the damage an explosion of size 6.0 (Endcrystal) would deal
-     * to the targeted EntityLivingBase.
+     * to the targeted EntityLivingBase. Note that beds(0.5, 0.5, 0.5) explode
+     * with a different offset than crystals(0.5, 1, 0.5) at their headpiece.
+     * Beds also don't create the same explosion size, but we ignore that for
+     * the sake of minDamage settings being the same for all calculations.
+     * (FeetPlace does same damage anyways).
      *
      * @param x the x coordinate of the position.
      * @param y the y coordinate of the position.
@@ -75,7 +83,6 @@ public class DamageUtil implements Globals
         }
         else
         {
-            //double density = base.world.getBlockDensity(new Vec3d(x, y, z), base.getEntityBoundingBox());
             double density = getBlockDensity(new Vec3d(x, y, z), base.getEntityBoundingBox(), true, true);
             double densityDistance = distance = (1.0D - distance) * density;
 
@@ -153,25 +160,13 @@ public class DamageUtil implements Globals
                         double yOff = bb.minY + (bb.maxY - bb.minY) * (double) b;
                         double zOff = bb.minZ + (bb.maxZ - bb.minZ) * (double) c;
 
-                        RayTraceResult result = mc.world.rayTraceBlocks(new Vec3d(xOff + xFloor, yOff, zOff + zFloor), vec);
+                        RayTraceResult result = rayTraceBlocks(new Vec3d(xOff + xFloor, yOff, zOff + zFloor), vec, false, false, false, ignoreWebs, ignoreBeds);
                         if (result == null)
                         {
-                            ++air;
-                        }
-                        else if (result.typeOfHit == RayTraceResult.Type.BLOCK)
-                        {
-                            Block block = mc.world.getBlockState(result.getBlockPos()).getBlock();
-                            if (ignoreWebs && block instanceof BlockWeb)
-                            {
-                                ++air;
-                            }
-                            else if (ignoreBeds && block instanceof BlockBed)
-                            {
-                                ++air;
-                            }
+                            air++;
                         }
 
-                        ++traced;
+                        traced++;
                     }
                 }
             }
@@ -181,6 +176,208 @@ public class DamageUtil implements Globals
         else
         {
             return 0.0F;
+        }
+    }
+
+    /**
+     * Essentially the same as {@link net.minecraft.world.World#rayTraceBlocks(Vec3d, Vec3d, boolean, boolean, boolean)} (Vec3d, AxisAlignedBB)}
+     * but with an option to ignore webs and beds.
+     *
+     * @param vec31 same as the original param.
+     * @param vec32 same as the original param.
+     * @param stopOnLiquid same as the original param.
+     * @param ignoreNoBox same as the original param.
+     * @param returnLastUncollidableBlock same as the original param.
+     * @param ignoreWebs handles webs like air.
+     * @param ignoreBeds handles beds like air.
+     * @return {@link net.minecraft.world.World#rayTraceBlocks(Vec3d, Vec3d, boolean, boolean, boolean)} (Vec3d, AxisAlignedBB)} for the given params.
+     */
+    public static RayTraceResult rayTraceBlocks(Vec3d vec31,
+                                                Vec3d vec32,
+                                                boolean stopOnLiquid,
+                                                boolean ignoreNoBox,
+                                                boolean returnLastUncollidableBlock,
+                                                boolean ignoreWebs,
+                                                boolean ignoreBeds)
+    {
+        if (!Double.isNaN(vec31.x) && !Double.isNaN(vec31.y) && !Double.isNaN(vec31.z))
+        {
+            if (!Double.isNaN(vec32.x) && !Double.isNaN(vec32.y) && !Double.isNaN(vec32.z))
+            {
+                int x1 = MathHelper.floor(vec31.x);
+                int y1 = MathHelper.floor(vec31.y);
+                int z1 = MathHelper.floor(vec31.z);
+                int x2 = MathHelper.floor(vec32.x);
+                int y2 = MathHelper.floor(vec32.y);
+                int z2 = MathHelper.floor(vec32.z);
+
+                BlockPos pos = new BlockPos(x1, y1, z1);
+                IBlockState state = mc.world.getBlockState(pos);
+                Block block = state.getBlock();
+
+                if ((!ignoreNoBox || state.getCollisionBoundingBox(mc.world, pos) != Block.NULL_AABB)
+                        && (block.canCollideCheck(state, stopOnLiquid)
+                        && !(ignoreBeds && block instanceof BlockBed || ignoreWebs && block instanceof BlockWeb)))
+                {
+                    RayTraceResult raytraceresult = state.collisionRayTrace(mc.world, pos, vec31, vec32);
+                    //noinspection ConstantConditions
+                    if (raytraceresult != null)
+                    {
+                        return raytraceresult;
+                    }
+                }
+
+                RayTraceResult raytraceresult2 = null;
+                int k1 = 200;
+
+                while (k1-- >= 0)
+                {
+                    if (Double.isNaN(vec31.x) || Double.isNaN(vec31.y) || Double.isNaN(vec31.z))
+                    {
+                        return null;
+                    }
+
+                    if (x1 == x2 && y1 == y2 && z1 == z2)
+                    {
+                        return returnLastUncollidableBlock ? raytraceresult2 : null;
+                    }
+
+                    boolean flag2 = true;
+                    boolean flag = true;
+                    boolean flag1 = true;
+                    double d0 = 999.0D;
+                    double d1 = 999.0D;
+                    double d2 = 999.0D;
+
+                    if (x2 > x1)
+                    {
+                        d0 = (double)x1 + 1.0D;
+                    }
+                    else if (x2 < x1)
+                    {
+                        d0 = (double)x1 + 0.0D;
+                    }
+                    else
+                    {
+                        flag2 = false;
+                    }
+
+                    if (y2 > y1)
+                    {
+                        d1 = (double)y1 + 1.0D;
+                    }
+                    else if (y2 < y1)
+                    {
+                        d1 = (double)y1 + 0.0D;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+
+                    if (z2 > z1)
+                    {
+                        d2 = (double)z1 + 1.0D;
+                    }
+                    else if (z2 < z1)
+                    {
+                        d2 = (double)z1 + 0.0D;
+                    }
+                    else
+                    {
+                        flag1 = false;
+                    }
+
+                    double d3 = 999.0D;
+                    double d4 = 999.0D;
+                    double d5 = 999.0D;
+                    double d6 = vec32.x - vec31.x;
+                    double d7 = vec32.y - vec31.y;
+                    double d8 = vec32.z - vec31.z;
+
+                    if (flag2)
+                    {
+                        d3 = (d0 - vec31.x) / d6;
+                    }
+
+                    if (flag)
+                    {
+                        d4 = (d1 - vec31.y) / d7;
+                    }
+
+                    if (flag1)
+                    {
+                        d5 = (d2 - vec31.z) / d8;
+                    }
+
+                    if (d3 == -0.0D)
+                    {
+                        d3 = -1.0E-4D;
+                    }
+
+                    if (d4 == -0.0D)
+                    {
+                        d4 = -1.0E-4D;
+                    }
+
+                    if (d5 == -0.0D)
+                    {
+                        d5 = -1.0E-4D;
+                    }
+
+                    EnumFacing enumfacing;
+
+                    if (d3 < d4 && d3 < d5)
+                    {
+                        enumfacing = x2 > x1 ? EnumFacing.WEST : EnumFacing.EAST;
+                        vec31 = new Vec3d(d0, vec31.y + d7 * d3, vec31.z + d8 * d3);
+                    }
+                    else if (d4 < d5)
+                    {
+                        enumfacing = y2 > y1 ? EnumFacing.DOWN : EnumFacing.UP;
+                        vec31 = new Vec3d(vec31.x + d6 * d4, d1, vec31.z + d8 * d4);
+                    }
+                    else
+                    {
+                        enumfacing = z2 > z1 ? EnumFacing.NORTH : EnumFacing.SOUTH;
+                        vec31 = new Vec3d(vec31.x + d6 * d5, vec31.y + d7 * d5, d2);
+                    }
+
+                    x1 = MathHelper.floor(vec31.x) - (enumfacing == EnumFacing.EAST ? 1 : 0);
+                    y1 = MathHelper.floor(vec31.y) - (enumfacing == EnumFacing.UP ? 1 : 0);
+                    z1 = MathHelper.floor(vec31.z) - (enumfacing == EnumFacing.SOUTH ? 1 : 0);
+                    pos = new BlockPos(x1, y1, z1);
+                    IBlockState state1 = mc.world.getBlockState(pos);
+                    Block block1 = state1.getBlock();
+
+                    if (!ignoreNoBox || state1.getMaterial() == Material.PORTAL || state1.getCollisionBoundingBox(mc.world, pos) != Block.NULL_AABB)
+                    {
+                        if (block1.canCollideCheck(state1, stopOnLiquid) && !(ignoreBeds && block1 instanceof BlockBed || ignoreWebs && block1 instanceof BlockWeb))
+                        {
+                            RayTraceResult raytraceresult1 = state1.collisionRayTrace(mc.world, pos, vec31, vec32);
+                            //noinspection ConstantConditions
+                            if (raytraceresult1 != null)
+                            {
+                                return raytraceresult1;
+                            }
+                        }
+                        else
+                        {
+                            raytraceresult2 = new RayTraceResult(RayTraceResult.Type.MISS, vec31, enumfacing, pos);
+                        }
+                    }
+                }
+
+                return returnLastUncollidableBlock ? raytraceresult2 : null;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        else
+        {
+            return null;
         }
     }
 
